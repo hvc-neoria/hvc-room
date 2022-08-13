d401b8f5750e3eed7900e43feb19933996dc1dbb
neoria <hvcharu@gmail.com>
2022-08-13 21:54:08 +0900

    メソッド抽出・JSDoc追加

--- miniTools/timestamper/code.js
+++ miniTools/timestamper/code.js
@@ -1,31 +1,66 @@
-const button = document.getElementsByClassName("btn")[0];
+// テーブルのレコード番号
+let recordNum = 1;
+// prevUnixTime変数の初期値
+const initialPrevUnixTime = 0;
+// 1つ前のレコードで発行したユニックスタイム
+let prevUnixTime = initialPrevUnixTime;
 
-let num = 1;
-// 前のタイムスタンプ時点でのユニックスタイム
-let prevUnixTime = 0;
-
+/**
+ * クリック時のメソッド
+ */
 const onclick = _ => {
     const tbody = document.getElementsByTagName("tbody")[0];
     const tr = document.createElement("tr");
     tbody.appendChild(tr);
 
-    const numElement = document.createElement("th");
-    numElement.scope = "row";
-    numElement.textContent = num++;
-    tr.appendChild(numElement);
-
-    const timestampTd = document.createElement("td");
+    tr.appendChild(createRecordNumTh());
     const currentDatetime = new Date();
-    timestampTd.textContent = `${currentDatetime.getHours()}:${currentDatetime.getMinutes()}:${currentDatetime.getSeconds()}`;
-    tr.appendChild(timestampTd);
+    tr.appendChild(createTimestampTd(currentDatetime));
+    tr.appendChild(createElapsedTimeTd(currentDatetime));
 
+    moveScrollbarToBottom();
+}
+
+const button = document.getElementsByClassName("btn")[0];
+button.onclick = onclick;
+
+/**
+ * レコード番号テーブルヘッダーを返す
+ * 生成する度にインクリメントする
+ * @returns レコード番号テーブルヘッダー
+ */
+const createRecordNumTh = _ => {
+    const recordNumTh = document.createElement("th");
+    recordNumTh.scope = "row";
+    recordNumTh.textContent = recordNum++;
+    return recordNumTh;
+}
+
+/**
+ * タイムスタンプテーブルデータを返す
+ * @param {string} currentDatetime new Date()で出力される現在日時を渡す
+ * @returns タイムスタンプテーブルデータ
+ */
+const createTimestampTd = currentDatetime => {
+    const timestampTd = document.createElement("td");
+    timestampTd.textContent = `${currentDatetime.getHours()}:${currentDatetime.getMinutes()}:${currentDatetime.getSeconds()}`;
+    return timestampTd;
+}
+
+/**
+ * 1つ前のタイムスタンプから現在のタイムスタンプまでの経過時間が記載されたテーブルデータを返す
+ * @param {string} currentDatetime new Date()で出力される現在日時を渡す
+ * @returns 経過時間テーブルデータ
+ */
+const createElapsedTimeTd = currentDatetime => {
     const elapsedTimeTd = document.createElement("td");
 
     // 現在のユニックスタイム
     // JavaScriptの現在日時はミリ秒だが、当ツールでは秒以上で取扱いたいため、
     // 秒に変換している
     const currentUnixTime = Math.floor(currentDatetime.getTime() / 1000)
-    if (prevUnixTime === 0) {
+
+    if (prevUnixTime === initialPrevUnixTime) {
         elapsedTimeTd.textContent = `-`;
     } else {
         const diff = currentUnixTime - prevUnixTime;
@@ -34,18 +69,15 @@ const onclick = _ => {
         const hours = Math.floor(diff / (60 * 60) % 60);
         elapsedTimeTd.textContent = `${hours}h${minutes}m${seconds}s`;
     }
-    tr.appendChild(elapsedTimeTd);
+    
     prevUnixTime = currentUnixTime;
+    return elapsedTimeTd;
+}
 
-    // スクロールバーの位置を最下部にする
+/**
+ * スクロールバーの位置を最下部にする
+ */
+const moveScrollbarToBottom = _ => {
     const container = document.getElementsByClassName("table-container")[0];
     container.scrollTo(0, container.scrollHeight);
 }
-
-// const appendTimestamp = (currentDatetime, tr) => {
-//     const timestampTd = document.createElement("td");
-//     timestampTd.textContent = `${currentDatetime.getHours()}:${currentDatetime.getMinutes()}:${currentDatetime.getSeconds()}`;
-//     tr.appendChild(timestampTd);
-// }
-
-button.onclick = onclick;
